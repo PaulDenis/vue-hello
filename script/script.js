22c670f0b3376a97cb7f0861a9c89cd885c9d3bd
GodDenix <lubbacknight@gmail.com>
2021-05-17 17:17:01 +0200

    Modifica per fare diventare l'oggeto image stringa

--- script/script.js
+++ script/script.js
@@ -2,16 +2,14 @@ var app = new Vue ({
     el: "#root",
     data: {
         message: "Hello World with Vue!",
-        image: {
-            img: "img/vue_logo.webp"
-        }
+        image: "img/vue_logo.webp"
     },
     methods: {
         cambia_img: function() {
-            if (this.image.img == "img/vue_logo.webp") {
-                this.image.img = "img/logo_vue_2.jpeg"
+            if (this.image == "img/vue_logo.webp") {
+                this.image = "img/logo_vue_2.jpeg";
             } else {
-                this.image.img = "img/vue_logo.webp";
+                this.image = "img/vue_logo.webp";
             };
         }
     }
