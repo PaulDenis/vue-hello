f3c8f09a7f41dd66c8ee6df197ea2d3dca879af9
PaulDenis <d.paul@outlook.it>
2021-05-17 15:08:32 +0200

    Aggiunto modo di cambiare img tramite click

--- script/script.js
+++ script/script.js
@@ -5,6 +5,15 @@ var app = new Vue ({
         image: {
             img: "img/vue_logo.webp"
         }
+    },
+    methods: {
+        cambia_img: function() {
+            if (this.image.img == "img/vue_logo.webp") {
+                this.image.img = "img/logo_vue_2.jpeg"
+            } else {
+                this.image.img = "img/vue_logo.webp";
+            };
+        }
     }
     
 });
